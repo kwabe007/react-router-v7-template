55011cc41df09d77a87bce414d0127a37c950ee5
Kwabena <kwabena.a92@gmail.com>
2026-02-01 01:19:34 +0100

    Import font in app.css

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,4 @@
-import * as Sentry from "@sentry/react-router";
 import {
-  isRouteErrorResponse,
   Links,
   Meta,
   Outlet,
@@ -14,19 +12,6 @@ import "./app.css";
 import { serverSideClientEnv } from "~/env.server";
 import { useEffect } from "react";
 import { RootErrorBoundary } from "~/root-components/RootErrorBoundary";
-
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
