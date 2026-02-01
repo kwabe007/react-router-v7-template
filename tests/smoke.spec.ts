c8d289948f483e7c0e78b1d76f81f88b9ee37824
Kwabena <kwabena.a92@gmail.com>
2026-02-01 20:51:12 +0100

    Add playwright

---
+++ tests/smoke.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("home page renders", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.locator("main")).toBeVisible();
+});
